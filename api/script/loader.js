7f4479812d60297b2802d379e5be1d0cd44ee964
Xynn-Caseoh <skibidibopbop119@gmail.com>
2025-06-27 12:45:26 +0700

    Update loader.js

--- api/script/loader.js
+++ api/script/loader.js
@@ -6,38 +6,36 @@ local H = game:GetService("HttpService")
 local P = game.Players.LocalPlayer
 
 local function makal_request(data)
-local R = (syn and syn.request) or (http and http.request) or (request) or (http_request) or (fluxus and fluxus.request) or (krnl and krnl.request)
-if R then
-return R(data)
-elseif data.Method == "GET" then
-return { Body = game:HttpGet(data.Url) }
-else
-error("Unsupported executor")
-end
+  local R = (syn and syn.request) or (http and http.request) or request or http_request or (fluxus and fluxus.request) or (krnl and krnl.request)
+  if R then
+    return R(data)
+  elseif data.Method == "GET" then
+    return { Body = game:HttpGet(data.Url) }
+  else
+    error("Unsupported executor")
+  end
 end
 
 local I = makal_request({
-Url = "https://makalhub.vercel.app/api/init",
-Method = "POST",
-Headers = {["Content-Type"] = "application/json"},
-Body = H:JSONEncode({ userid = P.UserId, username = P.Name })
+  Url = "https://makalhub.vercel.app/api/init?userid=" .. P.UserId .. "&username=" .. P.Name,
+  Method = "GET"
 })
 assert(I and I.Body, "Init failed")
 
 local T = H:JSONDecode(I.Body).token
 local M = {
-[537413528] = "babft",
-[109983668079237] = "stealabrainrot",
-[18687417158] = "forsaken"
+  [537413528] = "babft",
+  [109983668079237] = "stealabrainrot",
+  [18687417158] = "forsaken"
 }
 local N = M[game.PlaceId]
 assert(N, "Unsupported game")
 
 local S = makal_request({
-Url = "https://makalhub.vercel.app/api/script/" .. N,
-Method = "POST",
-Headers = {["Content-Type"] = "application/json"},
-Body = H:JSONEncode({name = N, token = T})
+  Url = "https://makalhub.vercel.app/api/script/" .. N,
+  Method = "POST",
+  Headers = { ["Content-Type"] = "application/json" },
+  Body = H:JSONEncode({ name = N, token = T })
 })
 assert(S and S.Body, "Script fetch failed")
 
